42d093e683a09d69230b8482da9e6130472e409b
unknown <George@George-HP.(none)>
2012-11-27 09:53:33 -0500

    Iteration 1: Protocol

--- source.java
+++ source.java
@@ -1,33 +1,54 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class myLabBuddy extends JFrame{
-	
+	BufferedImage background;
 	public myLabBuddy() {
 		initMenu();
 	}
+	
 	private static final long serialVersionUID = 1L;
 
 	public final void initMenu() {
 			JMenuBar menubar = new JMenuBar(); // www.clker.com/clipart-flask-icon.html
 			ImageIcon flask = new ImageIcon(getClass().getResource("/resources/flask_32x32.jpg"));
+			ImageIcon protocol = new ImageIcon(getClass().getResource("/resources/protocolAqua_32x32.jpg"));
 			//BufferedImage menuFlask = new BufferedImage(32, 32, BufferedImage.TYPE_INT_BGR);
 			
 			JMenu tasks = new JMenu("Tasks");
 			tasks.setMnemonic(KeyEvent.VK_T);
 			
-			JMenuItem labMenuItem = new JMenuItem("Make Solution - by molarity or percent", (Icon) flask);
-			labMenuItem.setMnemonic(KeyEvent.VK_S);
-			labMenuItem.setToolTipText("Calculate dry weight to add for x molar solution");
-			labMenuItem.addActionListener(new ActionListener() {
+			// tasks.menuSolution
+			JMenuItem menuTaskSolution = new JMenuItem("Make Solution - by molarity or percent", (Icon) flask);
+			menuTaskSolution.setMnemonic(KeyEvent.VK_S);
+			menuTaskSolution.setToolTipText("Calculate dry weight to add for x molar solution");
+			menuTaskSolution.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent event) {
 					makeSolution mySolution = new makeSolution();
 					mySolution.getParameters();
 				}
 			});
-			tasks.add(labMenuItem);
+			tasks.add(menuTaskSolution);
+			
+			// tasks.Protocol
+			JMenuItem menuTaskProtocol = new JMenuItem("Protocol - read a protocol", (Icon) protocol);
+			menuTaskProtocol.setMnemonic(KeyEvent.VK_P);
+			menuTaskProtocol.setToolTipText("Read a protocol");
+			menuTaskProtocol.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent event) {
+					Protocol myProtocol = new Protocol();
+					myProtocol.selectProtocol();
+					//myProtocol.???
+				}
+			});
+			tasks.add(menuTaskProtocol);
+			
 			menubar.add(tasks);
 			setJMenuBar(menubar);
 			setTitle("myLabBuddy - Life science solutions at your fingertips");
@@ -36,14 +57,14 @@ public class myLabBuddy extends JFrame{
 			setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 			
-		public static void main(String[] args) {
-			SwingUtilities.invokeLater(new Runnable() {
-	            public void run() {
-	                myLabBuddy MenuBuddy = new myLabBuddy();
-	                MenuBuddy.setVisible(true);
-	            }
-	        });
-		}
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                myLabBuddy MenuBuddy = new myLabBuddy();
+                MenuBuddy.setVisible(true);
+            }
+        });
+	}
 			
 		private class makeSolution{ // at this point only concentration in terms of molarity is accepted
 			private String FW;
@@ -64,10 +85,42 @@ public class myLabBuddy extends JFrame{
 				float molarity_float = Float.parseFloat(molarity);
 				float volume_float = Float.parseFloat(volume);
 				float grams = FW_float * molarity_float * volume_float;
-				JOptionPane.showMessageDialog(null, "Add " + grams + " grams of " + compound + " then bring the solution to a final volume of " + volume + " liter(s).");
+				ImageIcon flask = new ImageIcon(getClass().getResource("/resources/flask_32x32.jpg"));
+				JOptionPane.showMessageDialog(null, "Add " + grams + " grams of " + compound + " then bring the solution to a final volume of " + volume + " liter(s).", null,JOptionPane.INFORMATION_MESSAGE, flask);
 			}
 		}
-}
 		
-	
+		public class Protocol extends JFrame implements ActionListener{
+			public void selectProtocol() {
+					final String protocols[][] = {
+							{"Make solution", "If making a solution from a solid:\n1) Place the beaker on a stirrer and add an appropriately sized stir-bar.\n2) slowly dissolve the indicated amount in a beaker containing about one-half of the intended volume.\n3) Observe any additional solution tips that may be provided."}, 
+							{"pH a solution", "1) Wash probe with distilled water, dry probe.\n2) Calibrate the probe with a standard above and below the target pH."}, 
+							{"Sonicate tissue", "1) Clean the probe with distilled water and dab dry with lint-free wipe.\n2) Wear >20 dB reduction earmuffs.\n3) place probe in tissue/buffer mix and pulse - be sure not to allow the solution to froth!"} 
+					};
+					
+					String options[] =  {"Make solution", "pH a solution", "Sonicate tissue"};
+						
+					final JFrame frameProtocol = new JFrame();
+					JComboBox comboProtocol = new JComboBox(options);
 
+
+					comboProtocol.addActionListener(new ActionListener() {
+				    public void actionPerformed(ActionEvent e) {
+				    	JComboBox cp = (JComboBox)e.getSource();
+				    	int option = (int)cp.getSelectedIndex();
+				    	JOptionPane.showMessageDialog(null, protocols[option][1]);
+				    	System.out.println("Protocol worked!");
+				      }
+				    });
+					
+					frameProtocol.add(comboProtocol);
+					frameProtocol.setSize(300, 100);
+				    frameProtocol.setVisible(true);
+			}
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) { // TODO Auto-generated method stub
+				
+			}			
+		}
+}
